9febf24c6fda10c02b21a3294383fa84f651bebd
Artemiy Bondar <artemiy.bondar49@gmail.com>
2022-04-07 01:45:40 +0300

    14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = function Auth(req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthError('Необходима авторизация'));
+    return next(new AuthError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
