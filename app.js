d34c3d8fdc0c93c3c8c9e916cad6c242c178e963
Artemiy Bondar <artemiy.bondar49@gmail.com>
2022-04-14 20:35:45 +0300

    15

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const {
   login, createUser,
 } = require('./controllers/users');
 
+const { requestLogger, errorLogger } = require('./middlewares/Logger');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./Errors/NotFoundError');
@@ -17,6 +19,10 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://mesto.artem43.student.nomoredomains.xyz',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -24,6 +30,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+// app.use(cors);
+app.use(requestLogger);
 app.post('/signin', loginCheck, login);
 app.post('/signup', createUserCheck, createUser);
 
@@ -33,6 +41,7 @@ app.use('/', require('./routes/users'));
 
 app.use('/', require('./routes/cards'));
 
+app.use(errorLogger);
 app.use(errors());
 
 app.all('*', (req, res, next) => {
