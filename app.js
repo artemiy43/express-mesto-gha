424d33bd29b0dde2116ecb3422580f19a0857146
Artemiy Bondar <artemiy.bondar49@gmail.com>
2022-04-14 21:35:48 +0300

    17

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 const {
   login, createUser,
 } = require('./controllers/users');
@@ -19,10 +19,32 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://mesto.artem43.student.nomoredomains.xyz/',
-  credentials: true,
-}));
+const allowedCors = [
+  'https://mesto.artem43.student.nomoredomains.xyz',
+  'http://mesto.artem43.student.nomoredomains.xyz',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', requestHeaders);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
